a8ffa715d3666f0761223585d6c952d9c6ca9d30
Fikaroo <fuadbayramv@gmail.com>
2022-04-22 11:47:43 +0400

    major update

--- script.js
+++ script.js
@@ -1,1 +1,112 @@
-const url = 'https://api.exchangerate.host/latest?base=AZN&symbols=USD'
+let inCur, outCur;
+const url = 'https://api.exchangerate.host/latest';
+const myApi = fetch(`${url}?base=${inCur}&symbols=${outCur}`);
+const activeBtn = document.querySelectorAll('.btn-active');
+const btnIn = document.querySelectorAll('.input-select .btn-value');
+const btnOut = document.querySelectorAll('.output-select .btn-value');
+const curRateIn = document.querySelector('.input-area .currency-rate');
+const curRateOut = document.querySelector('.output-area .currency-rate');
+const inputIn = document.querySelector('.input-area .amount');
+const inputOut = document.querySelector('.output-area .amount');
+const errorTextIn = document.querySelector('.input .errorText');
+const errorNumIn = document.querySelector('.input .errorNum');
+const errorTextOut = document.querySelector('.output .errorText');
+const errorNumOut = document.querySelector('.output .errorNum');
+
+let curApiRateIn, curApiRateOut,inVal = 1;
+activeBtn.forEach((item, index) => {
+    if (index == 0)
+        inCur = item.value;
+    if (index == 1)
+        outCur = item.value;
+});
+
+
+
+const callApi = async () => {
+    if (inCur == outCur) {
+        curApiRateIn = 1;
+        curApiRateOut = 1;
+    }
+
+    const resIn = await fetch(`${url}?base=${inCur}&symbols=${outCur}`);
+    const resOut = await fetch(`${url}?base=${outCur}&symbols=${inCur}`);
+    const dataIn = await resIn.json();
+    const dataOut = await resOut.json();
+    curApiRateIn = await Object.values(dataIn.rates)[0];
+    curApiRateOut = await Object.values(dataOut.rates)[0];
+    appendRate();
+}
+
+callApi();
+
+function appendRate() {
+    curRateIn.textContent = `1 ${inCur} = ${curApiRateIn} ${outCur}`;
+    curRateOut.textContent = `1 ${outCur} = ${curApiRateOut} ${inCur}`;
+    inputOut.value = +(inputIn.value * curApiRateIn).toFixed(6);
+    inputIn.addEventListener('keyup', (e) => {
+        inVal = inputIn.value;
+        if (e.target.value == '') {
+            errorTextIn.style.display = 'none';
+            errorNumIn.style.display = 'none';
+            inputOut.value = '';
+        } else if (isNaN(e.target.value)) {
+            inputOut.value = '';
+            errorTextIn.style.display = 'block';
+        } else if (e.target.value.includes('-')) {
+            inputOut.value = '';
+            errorNumIn.style.display = 'block';
+        } else {
+            inputOut.value = +(e.target.value * curApiRateIn).toFixed(6);
+            errorTextIn.style.display = 'none';
+            errorNumIn.style.display = 'none';
+        }
+    });
+    inputOut.addEventListener('keyup', (e) => {
+        console.log(inputIn.value)
+        if (e.target.value == '') {
+            errorTextOut.style.display = 'none';
+            errorNumOut.style.display = 'none';
+            inputIn.value = '';
+        } else if (isNaN(e.target.value)) {
+            errorTextOut.style.display = 'block';
+            inputIn.value = '';
+        } else if (e.target.value.includes('-')) {
+            errorNumOut.style.display = 'block';
+            inputIn.value = '';
+        } else {
+            inputIn.value = +(e.target.value * curApiRateOut).toFixed(6);
+            errorTextOut.style.display = 'none';
+            errorNumOut.style.display = 'none';
+        }
+    })
+}
+
+eventListener();
+
+function eventListener() {
+    btnIn.forEach(item => item.addEventListener('click', changeCurIn));
+    btnOut.forEach(item => item.addEventListener('click', changeCurOut));
+}
+
+function changeCurIn(e) {
+    const activeBtnIn = document.querySelectorAll('.input-select .btn-active');
+    activeBtnIn.forEach(item => item.classList.remove('btn-active'));
+    e.target.classList.add('btn-active');
+    inCur = e.target.value;
+    inputIn.value = 1;
+    errorTextIn.style.display = 'none';
+    errorNumIn.style.display = 'none';
+    callApi();
+}
+
+function changeCurOut(e) {
+    const activeBtnOut = document.querySelectorAll('.output-select .btn-active');
+    activeBtnOut.forEach(item => item.classList.remove('btn-active'))
+    e.target.classList.add('btn-active');
+    outCur = e.target.value;
+    inputIn.value = inVal;
+    errorTextOut.style.display = 'none';
+    errorNumOut.style.display = 'none';
+    callApi();
+}
